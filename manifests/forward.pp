53af2389e8ccb4093935f5ac817707bb31e1e689
Matt Dainty <matt@bodgit-n-scarper.com>
2017-10-06 18:53:23 +0100

    Refactor unbound::forward define

--- manifests/forward.pp
+++ manifests/forward.pp
@@ -1,30 +1,35 @@
+# Define a forward zone in Unbound.
 #
+# @example Use Google as forwarders
+#   ::unbound::forward { '.':
+#     addr => [
+#       '8.8.8.8',
+#       '8.8.4.4',
+#     ],
+#   }
+#
+# @param zone
+# @param host
+# @param addr
+# @param first
+#
+# @see puppet_classes::unbound
+# @see puppet_defined_types::unbound::local
+# @see puppet_defined_types::unbound::stub
 define unbound::forward (
-  $host  = undef,
-  $addr  = undef,
-  $first = undef,
+  Bodgitlib::Zone                                  $zone  = $title,
+  Optional[Array[Bodgitlib::Hostname, 1]]          $host  = undef,
+  Optional[Array[Unbound::Interface::Incoming, 1]] $addr  = undef,
+  Optional[Boolean]                                $first = undef,
 ) {
 
   if ! defined(Class['::unbound']) {
-    fail('You must include the unbound base class before using any unbound defined resources') # lint:ignore:80chars
+    fail('You must include the unbound base class before using any unbound defined resources')
   }
 
-  validate_domain_name($name)
-  if $host {
-    validate_array($host)
-    validate_domain_name($host)
-  }
-  if $addr {
-    validate_array($addr)
-    validate_unbound_acl($addr)
-  }
-  if $first {
-    validate_bool($first)
-  }
-
-  ::concat::fragment { "unbound forward ${name}":
-    content => template('unbound/forward.erb'),
-    order   => "40-${name}",
+  ::concat::fragment { "unbound forward ${zone}":
+    content => template("${module_name}/forward.erb"),
+    order   => "40-${zone}",
     target  => "${::unbound::conf_dir}/unbound.conf",
   }
 }
