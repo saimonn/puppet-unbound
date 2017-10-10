eed7bf86b821cfa951fed1f5761da8b47c1b2cc8
Matt Dainty <matt@bodgit-n-scarper.com>
2017-10-10 06:58:14 +0100

    Split out Unbound::ACL::Action type alias
    Refactor existing Unbound::ACL type alias.

---
+++ spec/fixtures/test/manifests/acl/action.pp
@@ -0,0 +1,6 @@
+#
+class test::acl::action (
+  Unbound::ACL::Action $value,
+) {
+  notice('Success')
+}
--- types/acl.pp
+++ types/acl.pp
@@ -1,2 +1,2 @@
 # @since 2.0.0
-type Unbound::ACL = Tuple[IP::Address, Enum['allow', 'allow_snoop', 'deny', 'deny_non_local', 'refuse', 'refuse_non_local']]
+type Unbound::ACL = Tuple[IP::Address, Unbound::ACL::Action]
