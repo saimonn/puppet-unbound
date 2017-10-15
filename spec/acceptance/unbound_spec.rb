d275553dceea437e100956c6af14784d5adf2ebe
Matt Dainty <matt@bodgit-n-scarper.com>
2017-10-15 20:26:44 +0100

    Fix acceptance tests

--- spec/acceptance/unbound_spec.rb
+++ spec/acceptance/unbound_spec.rb
@@ -43,20 +43,43 @@ describe 'unbound' do
         domain_insecure => [
           'example.com.',
         ],
-      }
-
-      ::unbound::stub { 'example.com.':
-        addr => [
-          $::ipaddress,
+        local_zone      => [
+          ['example.org.', 'static'],
+        ],
+        local_data      => [
+          {
+            'name'     => 'example.org.',
+            'ttl'      => 86400,
+            'class'    => 'IN',
+            'type'     => 'NS',
+            'hostname' => 'ns1.example.org.',
+          },
+          {
+            'name'     => 'example.org.',
+            'ttl'      => 86400,
+            'class'    => 'IN',
+            'type'     => 'SOA',
+            'primary'  => 'ns1.example.org.',
+            'email'    => 'hostmaster.example.org.',
+            'serial'   => 1,
+            'refresh'  => 10800,
+            'retry'    => 15,
+            'expire'   => 604800,
+            'negative' => 10800,
+          },
+          {
+            'name'  => 'ns1.example.org.',
+            'ttl'   => 86400,
+            'class' => 'IN',
+            'type'  => 'A',
+            'ip'    => '192.168.1.1',
+          },
         ],
       }
 
-      ::unbound::local { 'example.org.':
-        type => 'static',
-        data => [
-          'example.org. 86400 IN NS ns1.example.org.',
-          'example.org. 86400 IN SOA ns1.example.org. hostmaster.example.org. 1 10800 15 604800 10800',
-          'ns1.example.org. 86400 IN A 192.168.1.1',
+      ::unbound::stub { 'example.com.':
+        stub_addr => [
+          $::ipaddress,
         ],
       }
 
@@ -75,15 +98,15 @@ describe 'unbound' do
     apply_manifest(pp, :catch_changes  => true)
   end
 
-  describe package('unbound'), :if => fact('osfamily') != 'OpenBSD' do
+  describe package('unbound'), :unless => fact('osfamily').eql?('OpenBSD') do
     it { should be_installed }
   end
 
-  describe file('/etc/sysconfig/unbound'), :if => fact('osfamily') == 'RedHat' do
+  describe file('/etc/sysconfig/unbound'), :if => fact('osfamily').eql?('RedHat') do
     it { should_not exist }
   end
 
-  describe file(conf_dir), :if => fact('osfamily') == 'OpenBSD' do
+  describe file(conf_dir), :if => fact('osfamily').eql?('OpenBSD') do
     it { should be_directory }
     it { should be_mode 755 }
     it { should be_owned_by 'root' }
@@ -127,32 +150,32 @@ describe 'unbound' do
 
   describe command('unbound-control status') do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match /version:\s+\d+\.\d+\.\d+/ }
-    its(:stdout) { should match /verbosity:\s+1/ }
-    its(:stdout) { should match /threads:\s+1/ }
-    its(:stdout) { should match /modules:\s+2\s+\[\s+validator\s+iterator\s+\]/ }
-    its(:stdout) { should match /uptime:\s+\d+\s+seconds?/ }
-    its(:stdout) { should match /unbound\s+\(pid\s+\d+\)\s+is\s+running\.\.\./ }
+    its(:stdout) { should match /version: \s+ \d+ \. \d+ \. \d+/x }
+    its(:stdout) { should match /verbosity: \s+ 1/x }
+    its(:stdout) { should match /threads: \s+ 1/x }
+    its(:stdout) { should match /modules: \s+ 2 \s+ \[ \s+ validator \s+ iterator \s+ \]/x }
+    its(:stdout) { should match /uptime: \s+ \d+ \s+ seconds?/x }
+    its(:stdout) { should match /unbound \s+ \( pid \s+ \d+ \) \s+ is \s+ running \.{3}/x }
   end
 
   describe command('dig @127.0.0.1 version.server txt chaos +time=1 +tries=1') do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match /version\.server\.\s+0\s+CH\s+TXT\s+"unbound\s+\d+\.\d+\.\d+"/ }
+    its(:stdout) { should match /version \. server \. \s+ 0 \s+ CH \s+ TXT \s+ "unbound \s+ \d+ \. \d+ \. \d+ "/x }
   end
 
   describe command('dig @127.0.0.1 com soa +dnssec +time=1 +tries=1') do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match /opcode:\s+QUERY,\s+status:\s+NOERROR/ }
-    its(:stdout) { should match /;;\s+flags:\s+qr\s+rd\s+ra\s+ad;/ }
+    its(:stdout) { should match /opcode: \s+ QUERY , \s+ status: \s+ NOERROR/x }
+    its(:stdout) { should match /;; \s+ flags: \s+ qr \s+ rd \s+ ra \s+ ad;/x }
   end
 
   describe command('dig @127.0.0.1 example.com soa +time=1 +tries=1') do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match /example\.com\.\s+86400\s+IN\s+SOA\s+ns1\.example\.com\.\s+hostmaster\.example\.com\.\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+/ }
+    its(:stdout) { should match /example \. com \. \s+ 86400 \s+ IN \s+ SOA \s+ ns1 \. example \. com \. \s+ hostmaster \. example \. com \. (?: \s+ \d+ ){5}/x }
   end
 
   describe command('dig @127.0.0.1 example.org soa +time=1 +tries=1') do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match /example\.org\.\s+86400\s+IN\s+SOA\s+ns1\.example\.org\.\s+hostmaster\.example\.org\.\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+/ }
+    its(:stdout) { should match /example \. org \. \s+ 86400 \s+ IN \s+ SOA \s+ ns1 \. example \. org \. \s+ hostmaster \. example \. org \. (?: \s+ \d+ ){5}/x }
   end
 end
